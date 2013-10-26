4e8a85da641526b9772853e8bba3b74574d0070c
Elias Kuiter <info@elias-kuiter.de>
2013-10-27 00:16:38 +0200

    Initial commit

---
+++ i586-elf-binutils.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class I586ElfBinutils < Formula
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
+
+  depends_on 'gcc48' => :build
+
+  def install
+    ENV['CC'] = '/usr/local/bin/gcc-4.8'
+    ENV['CXX'] = '/usr/local/bin/g++-4.8'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.8'
+    ENV['LD'] = '/usr/local/bin/gcc-4.8'
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=i586-elf',
+                             "--prefix=#{prefix}"
+      system 'make all'
+      system 'make install'
+      FileUtils.mv lib, libexec
+    end
+  end
+
+end
